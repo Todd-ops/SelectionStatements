1eaa6cad2dff454e73340e2cba6e36ea6b537bda
Todd Patton <64681278+Todd-ops@users.noreply.github.com>
2020-06-22 20:11:18 -0500

    inital commit

--- SelectionStatements/Program.cs
+++ SelectionStatements/Program.cs
@@ -6,7 +6,28 @@ namespace SelectionStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Guess a number between 1 - 100");
+            var r = new Random();
+
+            var favNumber = r.Next(1, 100);
+
+            
+
+            var userInput = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Guess a number between 1 - 100");
+
+            if (userInput == favNumber)
+            {
+                Console.WriteLine("Lucky Guess!");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too High");
+            }
+            else 
+            {
+                Console.WriteLine("Too Low!");
+            }
         }
     }
 }
